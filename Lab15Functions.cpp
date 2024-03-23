d1aef5bad88d24bb3e8ae85ef1ee23616ce9c452
KingHenryIX <henryj51505@gmail.com>
2024-03-23 12:07:14 -0400

    should work

--- Lab15Functions.cpp
+++ Lab15Functions.cpp
@@ -3,8 +3,34 @@
 * Gets and validates lottery info from the user and places it in   *
 * reference parameters referencing variables in the main function. *
 *******************************************************************/
-void getInfo(int & pickFrom, int & numPicks){
+#include <iostream>
+using namespace std;
 
+double factorial(int n);
+
+
+void getInfo(int & pickFrom, int & numPicks){
+do{
+cin.clear();
+cout << "How many balls (1-12) are in the pool to pick from?";
+cin >> pickFrom;
+if(cin.fail() or pickFrom <1 or pickFrom >12){
+    cin.fail() ? cout << "Input Error!\n" : cout << "Input Error! There must be between 1 and 12 balls.\n";
+
+    cin.ignore(100, '\n');
+}
+} while (cin.fail());
+
+do{
+cin.clear();
+cout << "How many balls (1-7) will be drawn?";
+cin >> numPicks;
+if(cin.fail() or numPicks <1 or numPicks >7){
+    cin.fail() ? cout << "Input Error!\n" : cout << "Input Error! There must be between 1 and 7 balls.\n";
+
+    cin.ignore(100, '\n');
+}
+} while (cin.fail());
 }
 
 /******************************************************************
@@ -14,15 +40,43 @@ void getInfo(int & pickFrom, int & numPicks){
 * The formula for this is     k!(n- k)!                           *
 *                             --------                            *
 *                                 n!                              *
+
+How many balls (1-12) are in the pool to pick from? 7
+How many balls (1-7) will be drawn? 9
+Input Error! 
+How many balls (1-7) will be drawn? 3
+
+Probability of winning is 0.0286
+Odds of winning are 1 in 35
+
+Would you like to calculate the probability of another scenario? (y/n): y
+
+How many balls (1-12) are in the pool to pick from? 14
+Input Error! There must be between 1 and 12 balls.
+How many balls (1-12) are in the pool to pick from? 12
+How many balls (1-12) will be drawn? red
+Input Error! 
+How many balls (1-12) will be drawn? 5
+
+Probability of winning is 0.0013
+Odds of winning are 1 in 791
+
+Would you like to calculate the probability of another scenario? (y/n): n
 *******************************************************************/
 // Note that the computation is done in a way that does not require
 // multiplying two factorials together. This is done to prevent any
 // intermediate result becoming so large that it causes overflow.
 double computeWays(int n, int k){
-    
+    double result = factorial(n) / (factorial(k) * factorial(n - k));
+    cout << result;
+    return 1 / result;
 }
 
 // This function computes factorials recursively. It is called by computeWays.
 double factorial(int n){
-
+    if(n == 0 or n == 1){
+        return 1;
+    } else{
+        return n * factorial (n-1);
+    }
 }
